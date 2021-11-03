00250d4f10e9c6a4b2310cf45dc276eaccf8db3d
Ilona Shishov <Ilona.Shishov@gmail.com>
2021-11-03 17:11:02 +0200

    deleted target files

--- mercury-framework/mercury-sd-archetype/target/classes/archetype-resources/__rootArtifactId__-common/src/main/java/__sdNameLowerCase__/services/__sdName__Service.java
+++
@@ -1,6 +0,0 @@
-package ${package}.${sdNameLowerCase}.services;
-
-public abstract class ${sdName}Service {
-
-    //TODO: Implement queries and commands
-}
--- mercury-framework/service-domains/customer-offer/customer-offer-common/target/quarkus-grpc17375705503352734805.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-"/usr/lib/jvm/java-11-openjdk-11.0.13.0.8-1.fc34.x86_64/bin/java" -cp "/home/Ilonas/.m2/repository/io/quarkus/quarkus-grpc-protoc-plugin/2.3.0.Final/quarkus-grpc-protoc-plugin-2.3.0.Final-shaded.jar" io.quarkus.grpc.protoc.plugin.MutinyGrpcGenerator
