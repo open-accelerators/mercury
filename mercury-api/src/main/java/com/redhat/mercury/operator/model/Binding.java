6ec5aaf37483e740c56c0eeebb3eb6528a4b92be
tmihalac <tmihalac@redhat.com>
2021-11-25 14:21:17 +0200

    Refactored code and crds following latest design change Updated tests

--- mercury-api/src/main/java/com/redhat/mercury/operator/model/Binding.java
+++ mercury-api/src/main/java/com/redhat/mercury/operator/model/Binding.java
@@ -2,12 +2,15 @@ package com.redhat.mercury.operator.model;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import io.fabric8.kubernetes.api.model.EnvVar;
 import io.sundr.builder.annotations.Buildable;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 import lombok.experimental.Accessors;
+
+import java.util.List;
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
 @JsonDeserialize
@@ -17,6 +20,12 @@ import lombok.experimental.Accessors;
 @Setter
 @Accessors(chain = true)
 @Buildable(editableEnabled = false, lazyCollectionInitEnabled = false, builderPackage = "io.fabric8.kubernetes.api.builder")
-public class ServiceDomainClusterSpec {
-    private String bindingServiceImage;
+public class Binding {
+    public enum BindingType {
+        Rhpam
+    }
+
+    private String type;
+    private String endpoint;
+    private List<EnvVar> env;
 }
